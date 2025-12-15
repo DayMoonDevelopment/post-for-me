a8a71207ee9c8dd030ae3c0cd6425138cae4fcf1
Matthew Rothenberger <s.mattroth@gmail.com>
2025-12-15 13:58:49 -0500

    adding ratelimits

--- dashboard/app/lib/.server/unkey.constants.ts
+++ dashboard/app/lib/.server/unkey.constants.ts
@@ -1,3 +1,5 @@
+import type { RatelimitRequest } from "@unkey/api/models/components";
+
 export const UNKEY_ROOT_KEY = process.env?.UNKEY_ROOT_KEY || "";
 export const UNKEY_API_ID = process.env?.UNKEY_API_ID || "";
 
@@ -8,3 +10,18 @@ if (!UNKEY_ROOT_KEY || UNKEY_ROOT_KEY.trim() === "") {
 if (!UNKEY_API_ID || UNKEY_API_ID.trim() === "") {
   throw new Error("UNKEY_API_ID is not defined");
 }
+
+export const RATE_LIMITS: RatelimitRequest[] = [
+  {
+    name: "per_minute_use",
+    limit: 40,
+    duration: 60000,
+    autoApply: true,
+  },
+  {
+    name: "per_second_use",
+    limit: 5,
+    duration: 1000,
+    autoApply: true,
+  },
+];
