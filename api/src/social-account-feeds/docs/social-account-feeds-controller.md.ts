4fe8be6d0faecce9fc7111206f561378bd247f06
Matthew Rothenberger <s.mattroth@gmail.com>
2026-01-28 09:17:05 -0500

    updating docs

--- api/src/social-account-feeds/docs/social-account-feeds-controller.md.ts
+++ api/src/social-account-feeds/docs/social-account-feeds-controller.md.ts
@@ -6,5 +6,15 @@ Details will include:
  - Post information including caption, url, media, etc.. 
  - When passing **expand=metrics**, Metrics information including views, likes, follows, etc..
 
-Note: Currently only the following platforms are supported **Instagram**, **Facebook**, **TikTok**, **TikTok Business**, **Youtube**
+Note: Currently the following platforms are supported: 
+ - **Instagram**, may take up to 48 hours for some metrics to be avaialbe 
+ - **Facebook** 
+ - **TikTok**, consumer API exposes less analytics for more details connect through TikTok Business
+ - **TikTok Business**, 
+ - **Youtube**
+ - **Threads**, If using our Quickstart project then metrics are currently unavailable.
+ - **Twitter (X) **
+ - **Bluesky**, Bluesky does not expose views or impressions through their API.
+ - **Pinterest**
+ - **LinkedIn**, metrics are only available for company pages. LinkedIn has currently stopped giving permission for personal page analytics, we are on the waitlist for when they resume.
 `;
