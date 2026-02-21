c0fa64ffdf0e644716e40452bf39cf83f02acef4
Matthew Rothenberger <s.mattroth@gmail.com>
2026-02-21 14:25:03 -0500

    clean up

--- dashboard/app/routes/_protected.$teamId.$projectId.posts._index/route.action.ts
+++ dashboard/app/routes/_protected.$teamId.$projectId.posts._index/route.action.ts
@@ -4,30 +4,8 @@ import { withDashboardKey } from "~/lib/.server/api/api";
 import { withSupabase } from "~/lib/.server/supabase";
 import { API_URL } from "~/lib/.server/api/api.constants";
 
-import type { PostWithConnections } from "./_types";
-
-export interface PostsResponse {
-  data: PostWithConnections[];
-  pagination: {
-    page: number;
-    limit: number;
-    total: number;
-    totalPages: number;
-  };
-}
-
 export const action = withSupabase(
-  withDashboardKey(async ({ request, apiKey, params, supabase }) => {
-    const { teamId, projectId } = params;
-
-    if (!teamId) {
-      throw new Error("Team code is required");
-    }
-
-    if (!projectId) {
-      throw new Error("Project ID is required");
-    }
-
+  withDashboardKey(async ({ request, apiKey, supabase }) => {
     const currentUser = await supabase.auth.getUser();
 
     if (!currentUser.data?.user) {
