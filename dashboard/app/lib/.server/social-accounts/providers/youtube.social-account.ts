9e1ef72e609ea6d1a9a032c57a54fa3471406db4
Matthew Rothenberger <s.mattroth@gmail.com>
2026-04-03 12:44:57 -0400

    fixing yt account error

--- dashboard/app/lib/.server/social-accounts/providers/youtube.social-account.ts
+++ dashboard/app/lib/.server/social-accounts/providers/youtube.social-account.ts
@@ -20,10 +20,14 @@ export async function getYoutubeSocialProviderConnection({
   const oauth2Client = new google.auth.OAuth2(
     appCredentials.appId!,
     appCredentials.appSecret!,
-    redirectUri
+    redirectUri,
   );
   const { tokens } = await oauth2Client.getToken(code);
   oauth2Client.setCredentials(tokens);
+
+  if (!tokens.access_token) {
+    return [];
+  }
 
   const youtube = google.youtube({ version: "v3", auth: oauth2Client });
 
@@ -32,20 +36,26 @@ export async function getYoutubeSocialProviderConnection({
     mine: true,
   });
 
-  const profilePhotoUrl =
-    userInfo.items?.[0]?.snippet?.thumbnails?.high?.url ||
-    userInfo.items?.[0]?.snippet?.thumbnails?.default?.url;
+  if (!userInfo.items || userInfo.items.length === 0) {
+    return [];
+  }
 
-  return [
-    {
-      social_provider_user_id: userInfo.items?.[0]?.id ?? "",
-      social_provider_user_name: userInfo.items?.[0]?.snippet?.title ?? "",
-      social_provider_photo_url: profilePhotoUrl ?? undefined,
-      access_token: tokens.access_token ?? "",
-      refresh_token: tokens.refresh_token ?? "",
-      access_token_expires_at: new Date(
-        Date.now() + (tokens.expiry_date ?? 1000)
-      ),
-    },
-  ];
+  return userInfo.items
+    .filter((c) => c.id)
+    .map((channel) => {
+      const profilePhotoUrl =
+        channel.snippet?.thumbnails?.high?.url ||
+        channel.snippet?.thumbnails?.default?.url;
+
+      return {
+        social_provider_user_id: channel.id,
+        social_provider_user_name: channel.snippet?.title ?? "",
+        social_provider_photo_url: profilePhotoUrl ?? undefined,
+        access_token: tokens.access_token!,
+        refresh_token: tokens.refresh_token ?? "",
+        access_token_expires_at: new Date(
+          Date.now() + (tokens.expiry_date ?? 1000),
+        ),
+      };
+    });
 }
